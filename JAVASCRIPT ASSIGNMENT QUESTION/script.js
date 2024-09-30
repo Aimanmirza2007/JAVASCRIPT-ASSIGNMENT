46b676368f5b18b119f659a73fcec3cf1c53bde8
Aimanmirza2007 <sm501623@gmail.com>
2024-09-30 22:37:58 +0500

    Add files via upload

---
+++ JAVASCRIPT ASSIGNMENT QUESTION/script.js
@@ -0,0 +1,52 @@
+//CHAPTER 01
+//ALERT
+//QUESTION N01
+
+//alert is a pop up when user enter a website to diplay alert like welcome to website , what's your name .The alert 
+//is also used to take input from user
+
+//QUESTION N02
+alert("Welcome to my Website!")
+
+//QUESTION N03
+
+//If you capitalize the alert keyword the error is generated and alert is not work alert must be written in lowercase
+
+//QUESTION N04
+
+alert("Hello,world!")
+
+//QUESTION N05
+alert("Hey viewer!")
+
+//alert must be written all small letter
+//always using round brakets () not use square[] and curly{} brakets
+//before you write a messege in text must using double "" or single quote''
+// you can also use an exclamation mark or  semicolon
+//the user must click on ok button
+
+//CHAPTER NO 02
+//QUESTION NO 01
+
+var messege = "Hello, world!"
+alert(messege);
+
+//QUESTION NO 02
+
+var age =
+age = 25
+alert(age)
+
+//CHAPTER NO 03
+
+var greeting = "Good morning!"
+alert(greeting)
+
+greeting = "Good evening!"
+alert(greeting);
+
+//CHAPTER NO 04
+
+var favoriteColor = "blue"
+alert(`my favorite Color is ${favoriteColor}`);
+
